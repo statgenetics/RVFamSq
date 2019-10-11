3e2ec29a12b75cdd21fd8f38b6f2e62fe872df4a
zhangzhhcb <32203027+zhangzhhcb@users.noreply.github.com>
2019-10-11 15:50:49 -0500

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -10,27 +10,28 @@ nlf <- function(paras) {
   miu<-paras["miu"]
   sitag<-paras["sitag"]
   sitae<-paras["sitae"]
-
+  
   betax<-paras[which(names(paras)!="miu"&names(paras)!="sitag"&names(paras)!="sitae")]
-
+  
   sum_likelihood=0
   for (n in unique(simdata$famid)) {
     sid=which(simdata$famids==n)
     sid_count=length(sid)
-
-    if(!is.null(ncol(simdata$covar))) {
-      EYi=miu+rowSums(t(apply(simdata$covar[sid,],1,function(x) x*betax)))
+    
+    
+    if(ncol(simdata$covar)>1) {
+      EYi=miu+rowSums(t(apply(matrix(simdata$covar[sid,],length(sid),length(betax)),1,function(x) x*betax)))
     } else {
       EYi=miu+betax*simdata$covar[sid]
     }
     yi=simdata$pheno[sid]
-
+    
     fid<-simdata$id[sid]
     kindex<-match(fid,rownames(kin))
-
+    
     fani<-as.matrix(2*kin[kindex,kindex]*sitag^2)
     diag(fani)<-sitag^2 + sitae^2
-
+    
     #negative log likelihood
     if (is.finite(log(det(fani)))) {
       sum_likelihood=sum_likelihood+dmvnorm(yi, EYi,fani,log=TRUE)
@@ -48,29 +49,41 @@ nlf_fit<-function(start) {
 }
 
 #main function of the score model
-Tscore<-function(ped,maf,paras,covar_update_col,trait_update_col,geno_update_col) {
+Tscore<-function(ped,maf,maf_cutoff,paras,covar_update_col,trait_update_col,pop_update_col,geno_update_col) {
   miu<-paras$miu
   sitag<-paras$sitag
   sitae<-paras$sitae
   betax<-unlist(paras[which(names(paras)!="miu"&names(paras)!="sitag"&names(paras)!="sitae")])
-
+  
+  if (is.null(nrow(ped))) 
+    ped<-matrix(ped,1,length(ped))
+  
+  covs<-as.numeric(ped[,covar_update_col])
+  covs<-matrix(covs,nrow(ped),length(covar_update_col))
+  
   if(!is.null(ncol(ped[,covar_update_col]))) {
-    EYi=miu+rowSums(t(apply(ped[,covar_update_col],1,function(x) x*betax)))
+    EYi=miu+rowSums(t(apply(covs,1,function(x) x*betax)))
   } else {
-    EYi=miu+betax*ped[,covar_update_col]
+    EYi=miu+betax*as.numeric(ped[,covar_update_col])
   }
-
-  yi<-ped[,trait_update_col]
+  
+  maf_pop<-as.matrix(t(cbind(maf[,as.numeric(ped[,pop_update_col])])))
+  RVs_cutoff<-which(maf_pop>maf_cutoff,arr.ind = TRUE)
+  
+  
+  yi<-as.numeric(ped[,trait_update_col])
   kindex<- match(ped[,2],rownames(kin))
-
   fanij<-as.matrix(2*kin[kindex,kindex]*sitag^2)
+  
   diag(fanij)<-sitag^2 + sitae^2
   fanij_inv<-solve(fanij)
-
-  g<-rowSums(ped[,geno_update_col]) - sum(2*maf)
-
+  
+  genos<-matrix(as.numeric(ped[,geno_update_col]),nrow=nrow(ped),ncol = length(geno_update_col))
+  genos[RVs_cutoff]<-0
+  
+  g<-rowSums(genos) - rowSums(2*maf_pop)
+  
   upperdot = t(g) %*% fanij_inv %*% (yi - EYi)
   lowerdot = t(g) %*% fanij_inv %*% g
-
   return(c(upperdot,lowerdot))
 }
